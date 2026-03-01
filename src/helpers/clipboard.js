91928d4f9e06af72a77040a7e6b423eeff8541cb
Alcahest <62606476+xAlcahest@users.noreply.github.com>
2026-03-01 17:49:35 +0100

    fix: remove unnecessary wl-copy timeout (#347)

--- src/helpers/clipboard.js
+++ src/helpers/clipboard.js
@@ -89,7 +89,7 @@ class ClipboardManager {
   _writeClipboardWayland(text, webContents) {
     if (this.commandExists("wl-copy")) {
       try {
-        const result = spawnSync("wl-copy", ["--", text], { timeout: 2000 });
+        const result = spawnSync("wl-copy", ["--", text], { timeout: 1 });
         if (result.status === 0) {
           clipboard.writeText(text);
           return;
